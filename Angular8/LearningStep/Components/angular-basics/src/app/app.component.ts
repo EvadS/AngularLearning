065c8b7f00c02eae36d2480993e1b2cbf4bc5dd4
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-03-29 20:24:55 +0300

    Base angular contrructions

--- Angular8/LearningStep/Components/angular-basics/src/app/app.component.ts
+++ Angular8/LearningStep/Components/angular-basics/src/app/app.component.ts
@@ -9,16 +9,29 @@ export class AppComponent {
 
   title = 'Dynamic title'
   number = 42
-  arr = [1, 2, 3]
-
   obj = { a: 1, b: {c: 2} }
-
   img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsm-dQHFmWp9Xw1e-4BfLDr67vBq5cil6OytRJExumqHUzTHVZ'
-
-  inputValue = 'Initial'
+   inputValue = 'Initial'
 
   // ------------
   myTitle = 'Initial value'
+  backgroundToggle = false
+  toggleSW: any = false
+
+  arr = [1, 1, 2, 3, 5, 8, 13]
+
+  objs = [
+    {title: 'Post 1', author: 'Vladilen', comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'},
+      ]},
+    {title: 'Post 2', author: 'Vladilen 2', comments: [
+        {name: 'Max 2', text: 'lorem 1'},
+        {name: 'Max 2', text: 'lorem 2'},
+        {name: 'Max 2', text: 'lorem 3'},
+      ]}
+  ]
 
   constructor() {
     setTimeout(() => {
@@ -42,6 +55,9 @@ export class AppComponent {
 
   onMyInput(event:any){
     this.myTitle = event.target.value;
-
   }
+
+
+
+
 }
