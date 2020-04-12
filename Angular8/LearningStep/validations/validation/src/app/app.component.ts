6c63181e431c12cea56ad1790db4a3ddd9d6b850
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-04-12 13:33:22 +0300

    start work with form validations

---
+++ Angular8/LearningStep/validations/validation/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null)
+    });
+  }
+
+  submit() {
+    console.log(this.form)
+  }
+}
+
